bd349fd075e61d557d78ac84fd1d4f2d29dced41
Samuel wanza <s.wanza@alustudent.com>
2022-04-18 23:18:35 +0200

    Updates

--- Testcases.py
+++ Testcases.py
@@ -12,11 +12,11 @@ class TestMAAT_STOCK_OPTIMISATION_SYSTEM(unittest.TestCase):
         self._start_app()
 
     def test_knapsack_algorithm(self):
-        k = 10
-        g = [1, 5, 9, 0]
-        l = [10, 50, 60, 40]
+        weight_limit = 10
+        weights= [1, 5, 9, 0]
+        values = [10, 50, 60, 40]
 
-        App = MAAT_STOCK_OPTIMISATION_SYSTEM().knapsack_algorithm(k, g, l, 4)
+        App = MAAT_STOCK_OPTIMISATION_SYSTEM().knapsack_algorithm(weight_limit, weights, values, 4)
         self.assertTrue(App,True)
     def test_portfolio(self):
         App=MAAT_STOCK_OPTIMISATION_SYSTEM()
